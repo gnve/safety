5cb6e7992bc4a9f8ce4c3fcb3b045ee1504e6c31
gene r <genevereyes@gmail.com>
2023-02-13 21:07:11 -0800

    Original files, first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("safetytips.csv")
+  .then(response => response.text())
+  .then(data => {
+    // Parse the CSV data into an array of objects
+    var safetyTips = Papa.parse(data, { header: true, dynamicTyping: true }).data;
+
+    var safetyTipElement = document.getElementById("safety-tip");
+    var retryButton = document.getElementById("retry-button");
+
+    function displayRandomTip() {
+      var randomTip = safetyTips[Math.floor(Math.random() * safetyTips.length)];
+      safetyTipElement.innerHTML = "<h2>" + randomTip.Tip + "</h2><p>" + randomTip.Description + "</p>";
+    }
+
+    retryButton.addEventListener("click", displayRandomTip);
+
+    // Display a random tip when the page loads
+    displayRandomTip();
+  });
